bf7382846d6c29b462066ccef573a846701b7fc4
true2000 <57641483+true2000@users.noreply.github.com>
2023-11-07 17:14:18 -0600

    test

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,4 +1,45 @@
 console.log("test")
+function getSPFromCardName(card) {
+  const p = /\((\d+)\)/
+  const m = card.match(p)
+  if (m)
+      return Number(m[1])
+
+  return 0
+}
+
+function setColumnName(id, name) {
+  Trello.put(`/lists/${id}`, {
+      name: name
+  })
+}
+
+
+_COLUMN_REGEX = [/^Completed.*/, /^Next.*/, /^In Progress.*/];
+
+(function() {
+
+  Trello.board.get().then((board)=>{
+
+      const lists = board.lists
+
+      lists.forEach(async(list)=>{
+          if (list.closed || !_COLUMN_REGEX.some(p=>p.test(list.name)))
+              return
+
+          const cards = await Trello.lists.get(`${list.id}/cards`)
+          const storyPoints = cards.map(card=>Number(getSPFromCardName(card.name)))
+
+          const total = storyPoints.reduce((a,b)=>a + b, 0)
+          const columnName = list.name.replace(/ \(Total SP: \d+\)/, '')
+
+          console.debug("Updating list: ", columnName, list)
+
+          setColumnName(list.id, `${columnName} (Total SP: ${total})`)
+      })
+
+  })
+}())
 TrelloPowerUp.initialize({
     'card-buttons': function(t, options){
       return [{
@@ -16,13 +57,12 @@ TrelloPowerUp.initialize({
           callback: function (t) {
             // Trello will call this if the user clicks on this action
             // we could for example open a new popover...
-            t.popup({
-              // ...
-            });
           }
         }];
       });
     }
+
+    
     // When a card is removed from the list
     // list total name will update by n - 1
 
