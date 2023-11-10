9fea64c3798171990fa1e8759b24124892358fa7
true2000 <57641483+true2000@users.noreply.github.com>
2023-11-10 15:22:24 -0600

    test build

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,4 +1,6 @@
 console.log("test")
+var numCards = 0
+var cardsComplete = 0
 TrelloPowerUp.initialize({
     'card-buttons': function(t, options){
       return [{
@@ -8,11 +10,15 @@ TrelloPowerUp.initialize({
     },
     // When a card is added to the list 
     // list total name will update by n + 1
-    'list-actions': function (t) {
-      return t.list('name', 'id')
+    'list-actions': function (l) {
+      return l.list('cards')
       .then(function (list) {
+          numCards = list.cards.length;
+          cardsComplete = list.cards.filter(function(card){
+              return card.dueComplete;
+          }).length
         return [{
-          text: "Get List Stats",
+          text: "Total Cards: "+ numCards + " Completed Cards: " + cardsComplete,
           callback: function (t) {
             // Trello will call this if the user clicks on this action
             // we could for example open a new popover...
