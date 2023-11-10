3121c4a106a85903cc70f905d40bd383f35ac9ad
true2000 <57641483+true2000@users.noreply.github.com>
2023-11-10 16:46:29 -0600

    test build

--- src/js/connector.js
+++ src/js/connector.js
@@ -2,40 +2,41 @@ console.log("test")
 var numCards = 0
 var cardsComplete = 0
 TrelloPowerUp.initialize({
+    // Testing code
     'card-buttons': function(t, options){
       return [{
         icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
         text: 'Estimate Size',
       }];
     },
-    // When a card is added to the list 
-    // list total name will update by n + 1
+    // Located on the bullet menu on each list
     'list-actions': function (l) {
-      return l.list('cards')
+      // Acesses the name, id and cards for each list
+      return l.list('name','id','cards')
       .then(function (list) {
+          // Finds the total number of cards for each list
           numCards = list.cards.length;
+          // Finds the amount of completed cards from each list
           cardsComplete = list.cards.filter(function(card){
               return card.dueComplete;
           }).length
+          // Create a local var that contains the new name
+          var newName = list.name + " ("+ numCards + " total) " + " ("+ cardsComplete + " complete)"
+        // Returns the following values
         return [{
-          text: "Total Cards: "+ numCards + " Completed Cards: " + cardsComplete,
-          callback: function (t) {
-            // Trello will call this if the user clicks on this action
-            // we could for example open a new popover...
-            t.popup({
-              // ...
-            });
+          // Title of the action in the bullet menu
+          text: "Total Cards: "+ numCards + " Completed Cards: " + cardsComplete + " New Name: " + newName,
+          // Trello will call this if the user clicks on this action
+          callback: function (l) {
+            // Prints what the new title should be in the console
+            console.log(newName)
+            // Should change the title of the list
+            // stuck having issues
+            //return l.list.name.set(newName);
           }
         }];
       });
     }
-
-    
-    // When a card is removed from the list
-    // list total name will update by n - 1
-
-    // When a card due date is complete
-    // list completed name will update by i + 1
 
     // At the start of each month have a new list appear that contains:
     // *month* *n* *n-i*
