71064171c53ddf600188730e83267919c7a36326
true2000 <57641483+true2000@users.noreply.github.com>
2023-11-17 16:40:15 -0600

    Test Build

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,6 +1,22 @@
-console.log('hello world')
-window.TrelloPowerUp.initialize({
-  'card-badges' : function(t, opts){
-    return[];
-  }
-})
+// Access name of list
+listNamesElement = "KLvU2mDGTQrsWG"
+// Access columns of names
+colContainerElement = "RD2CmKQFZKidd6"
+// Access cards of columns
+cardsElement = "T9JQSaXUsHTEzk"
+// Access completed of cards
+completeElement = "BOkIMdOUAQVOQk i0a3_aIBK1HcUl IRBlPpAzmf7j9R l2mAoxP747LETW"
+
+// Looks for all lists in the board
+listNames = document.getElementsByClassName(listNamesElement);
+// Looks at all columns in the board
+colContainer = document.getElementsByClassName(colContainerElement);
+// for loop iterates through each list name
+for(i = 0; i<listNames.length; i++){
+  // Gets the size of all the cards in the current column
+  total = colContainer[i].getElementsByClassName(cardsElement).length
+  // Gets the size of all the cards marked as complete in the current column
+  complete = colContainer[i].getElementsByClassName(completeElement).length
+  // Updates the html text with current values
+  listNames[i].textContent = "Total: "+total+"; Complete: "+complete;
+}
