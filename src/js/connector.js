acaa0f0d6413d6d16e9c0b66203adff4a2ad4c74
true2000 <57641483+true2000@users.noreply.github.com>
2023-11-17 14:49:42 -0600

    restarting

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,11 +1,6 @@
-// Get the Trello API client
-var trello = new Trello('96d4cc5b572cddd7e1334f73c48544c0');
-
-// Create a new filter
-var filter = {
-  name: 'My Custom Filter',
-  text: 'This is my custom filter text'
-};
-
-// Add the filter to Trello
-trello.addFilter(filter);
+console.log('hello world')
+window.TrelloPowerUp.initialize({
+  'card-badges' : function(t, opts){
+    return[];
+  }
+})
