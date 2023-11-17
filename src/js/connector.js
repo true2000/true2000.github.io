a96e2b48c73ff1eabbedf029340dba029f1f8454
true2000 <57641483+true2000@users.noreply.github.com>
2023-11-17 14:10:03 -0600

    changes to fetch

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,13 +1,20 @@
-var Trello = require("node-trello");
-var t = new Trello('96d4cc5b572cddd7e1334f73c48544c0', 'ATTA724953d7de3fa035c1bc7e117f8043548453603f5fe0068b683f2f7f3cdbfa32358D5658');
+const apiKey = '96d4cc5b572cddd7e1334f73c48544c0';
+const token = 'ATTA724953d7de3fa035c1bc7e117f8043548453603f5fe0068b683f2f7f3cdbfa32358D5658';
+const boardId = '7bZN9C3M/testing-trello-board';
 
-t.get("/1/boards/7bZN9C3M/testing-trello-board/lists", { cards: "open" }, function(err, lists) {
-  if (err) throw err;
-  lists.forEach(function(list) {
-    var cardCount = list.cards.length;
-    var newListName = list.name.replace(/\(\d+\)$/, '') + `(${cardCount})`;
-    t.put(`/1/lists/${list.id}/name`, { value: newListName }, function(err) {
-      if (err) throw err;
+fetch(`https://api.trello.com/1/boards/${boardId}/lists?cards=open&key=${apiKey}&token=${token}`)
+  .then(response => response.json())
+  .then(lists => {
+    lists.forEach(list => {
+      const cardCount = list.cards.length;
+      const newListName = list.name.replace(/\(\d+\)$/, '') + `(${cardCount})`;
+      fetch(`https://api.trello.com/1/lists/${list.id}/name?key=${apiKey}&token=${token}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ value: newListName })
+      });
     });
-  });
-});
+  })
+  .catch(error => console.error('Error:', error));
