502ad87d7b366da19ad18e9d7379159c385093ef
true2000 <57641483+true2000@users.noreply.github.com>
2023-11-20 16:17:52 -0600

    Tutorial 1

--- estimate.js
+++ estimate.js
@@ -1,7 +1,8 @@
+/* global TrelloPowerUp */
+
 var t = TrelloPowerUp.iframe();
 
 window.estimate.addEventListener("submit", function (event) {
-  // Stop the browser trying to submit the form itself.
   event.preventDefault();
   return t
     .set("card", "shared", "estimate", window.estimateSize.value)
@@ -11,5 +12,12 @@ window.estimate.addEventListener("submit", function (event) {
 });
 
 t.render(function () {
-  t.sizeTo("#estimate").done();
+  return t
+    .get("card", "shared", "estimate")
+    .then(function (estimate) {
+      window.estimateSize.value = estimate;
+    })
+    .then(function () {
+      t.sizeTo("#estimate").done();
+    });
 });
