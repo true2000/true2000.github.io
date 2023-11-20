b7e856065346ce59f28627c42ca3c468a235984e
true2000 <57641483+true2000@users.noreply.github.com>
2023-11-20 15:23:22 -0600

    react test

--- src/js/connector.js
+++ src/js/connector.js
@@ -7,7 +7,27 @@ cardsElement = "T9JQSaXUsHTEzk"
 // Access completed of cards
 completeElement = "BOkIMdOUAQVOQk i0a3_aIBK1HcUl IRBlPpAzmf7j9R l2mAoxP747LETW"
 
-// Looks for all lists in the board
+TrelloPowerUp.initialize({
+  'board-buttons': function(t, options){
+    return [{
+      icon: './images/icon.svg',
+      text: 'Update List Names',
+      callback: function(t){
+        return t.lists('all')
+        .then(function(lists){
+          lists.forEach(function(list){
+            var total = list.cards.length;
+            var complete = list.cards.filter(card => card.badges.checkItemsChecked > 0).length;
+            list.name = "Total: "+total+"; Complete: "+complete;
+            Trello.put('/lists/'+list.id, {name: list.name});
+          });
+        });
+      }
+    }];
+  }
+});
+
+/*// Looks for all lists in the board
 listNames = document.getElementsByClassName(listNamesElement);
 // Looks at all columns in the board
 colContainer = document.getElementsByClassName(colContainerElement);
@@ -19,4 +39,4 @@ for(i = 0; i<listNames.length; i++){
   complete = colContainer[i].getElementsByClassName(completeElement).length
   // Updates the html text with current values
   listNames[i].textContent = "Total: "+total+"; Complete: "+complete;
-}
+}*/
