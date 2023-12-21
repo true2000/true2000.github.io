15f8021038794495cc229f5b4ceebb40f51708f7
true2000 <57641483+true2000@users.noreply.github.com>
2023-12-21 15:46:39 -0600

    test

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,4 +1,4 @@
-/*TrelloPowerUp.initialize({
+TrelloPowerUp.initialize({
   "card-buttons": function (t, options) {
     return [
       {
@@ -16,96 +16,13 @@
         icon: "https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421",
         text: "Board Button",
         callback: function (t) {
-          return t.lists('id','name').then(card => alert('Hello from ' + card[1].name + '(id: ' + card[1].id + '!)'), card[1].name = 'ahi', alert(card.name))
-          //return t.board().then(board => alert('Hello World!'))
+          return t.lists("all").then(function (lists) {
+            console.log(JSON.stringify(lists, null, 2));
+          });
         },
       },
     ];
   },
-});*/
-
-// This is the code for the power up server
-// It uses the Express framework to handle the requests from Trello
-const express = window.express;
-const app = express();
-
-// This is the URL of the Trello client library, which we need to load in our power up
-const trelloClientUrl = "https://p.trellocdn.com/power-up.min.js";
-
-// This is the function that will be called when the board button is clicked
-// It uses the Trello client library to create a new card on the board
-const onBoardButtonClick = async (t) => {
-  // Get the board id and name from the power up context
-  const boardId = t.getContext().board;
-  const boardName = await t.board("name").get("name");
-
-  // Create a new card with a default title and description
-  // You can customize these values as you want
-  const cardTitle = "New card from power up";
-  const cardDescription = `This card was created by the power up on the board ${boardName}`;
-
-  // Use the Trello client library to create the card
-  // You can also specify other properties, such as list, labels, members, etc.
-  // See the documentation for more details: https://developer.atlassian.com/cloud/trello/guides/power-ups/client-library/#trello-client-js-library
-  const card = await t.post(`/cards`, {
-    name: cardTitle,
-    desc: cardDescription,
-    idBoard: boardId,
-  });
-
-  // Return a response to the power up
-  // You can either return a simple message, or a URL to redirect the user to the card
-  // See the documentation for more details: https://developer.atlassian.com/cloud/trello/guides/power-ups/capabilities/#board-buttons
-  return {
-    text: `Created card: ${cardTitle}`,
-    url: card.url,
-  };
-};
-
-// This is the main function that handles the power up initialization and requests
-// It uses the power up API to register the capabilities and settings of the power up
-// See the documentation for more details: https://developer.atlassian.com/cloud/trello/guides/power-ups/initializing/
-const powerUpHandler = (t, options) => {
-  return {
-    // This is the board button capability
-    // It defines the icon, text, and callback function of the board button
-    // See the documentation for more details: https://developer.atlassian.com/cloud/trello/guides/power-ups/capabilities/#board-buttons
-    "board-buttons": () => {
-      return [
-        {
-          icon: {
-            dark: `${t.signUrl("./images/icon-white.svg")}`,
-            light: `${t.signUrl("./images/icon-black.svg")}`,
-          },
-          text: "Create Card",
-          callback: onBoardButtonClick,
-        },
-      ];
-    },
-  };
-};
-
-// This is the endpoint that serves the power up client library
-app.use("/power-up.js", (req, res) => {
-  res.redirect(trelloClientUrl);
-});
-
-// This is the endpoint that serves the power up handler function
-app.use("/power-up", (req, res) => {
-  res.json(powerUpHandler);
-});
-
-// This is the endpoint that serves the power up icon
-app.use("/images", express.static("images"));
-
-// This is the endpoint that serves the power up index page
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
-
-// This is the code that starts the server and listens for requests
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
 });
 
 
